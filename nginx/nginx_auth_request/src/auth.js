8bc023811a939e27282c670db41ea29aed8a2cae
Somprasong Damyos <somprasong.damyos@gmail.com>
2022-08-22 20:03:42 +0700

    finish

--- nginx/nginx_auth_request/src/auth.js
+++ nginx/nginx_auth_request/src/auth.js
@@ -1,18 +1,19 @@
 const express = require('express');
+const cors = require('cors');
 const jwt = require('jsonwebtoken');
 
 const secret = 'secretjwt';
 
 function main() {
   const app = express();
-
+  app.use(cors());
   app.post('/api/auth/signin', (req, res) => {
     const isAdmin = req.query.isAdmin === 'true';
 
     const token = jwt.sign(
       {
         id: 1,
-        username: 'user',
+        username: isAdmin ? 'admin' : 'user',
         isAdmin,
       },
       secret
@@ -21,14 +22,21 @@ function main() {
     res.json({ token });
   });
 
-  app.post('/api/auth/verify', (req, res) => {
+  app.get('/api/auth/verify', (req, res) => {
     const token = req.headers.authorization
       ? req.headers.authorization.split(' ')
       : undefined;
     if (!token || token.length < 2) return res.sendStatus(401);
-    const user = jwt.verify(token[1], secret);
-    res.set('X-Id-Token', Buffer.from(JSON.stringify(user)).toString('base64'));
-    res.sendStatus(200);
+    try {
+      const user = jwt.verify(token[1], secret);
+      res.set(
+        'X-Id-Token',
+        Buffer.from(JSON.stringify(user)).toString('base64')
+      );
+      res.status(200).json(user);
+    } catch (error) {
+      res.sendStatus(401);
+    }
   });
 
   app.listen('3000', console.log('auth server start on port 3000'));
